4829b10ae1ff4fa9b94a2f4661f66896ee6f9b19
Carolina <carolina.torres.baroffio@gmail.com>
2023-11-09 11:11:03 -0300

    cambio en rutas

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->group(function () {
-    Route::get('/users', [AdminController::class, 'index']);
-    Route::get('/users/reported', [AdminController::class, 'indexReported']);
-    
-});
+
